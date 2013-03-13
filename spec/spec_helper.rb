815396e711211b1fcc1e171c4084779847b330f1
Josh Schairbaum <josh.schairbaum@rackspace.com>
2013-03-13 17:18:32 -0500

    Cleanup testing setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@
 # Require this file using `require "spec_helper"` to ensure that it is only
 # loaded once.
 #
+require 'alonzo'
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
